f645f603d3fd46f3f36c49dd88f2bd46a75888e4
luan0ap <luanap.kun@gmail.com>
2018-07-18 14:45:37 -0300

    refactored requests

--- src/requests.js
+++ src/requests.js
@@ -1,36 +1,28 @@
 const chalk = require('chalk');
 const fetch = require('node-fetch');
 
+const get = async URL =>
+  fetch(URL)
+    .then(res => res.json())
+    .then(data => data);
+
 const requestJoke = async () => {
-    const url = 'https://api.chucknorris.io/jokes/random';
-    const response = await fetch(url);
-    const data = await response.json();
-    return console.log(chalk.white.bgGreen.bold(data.value));
+  const URL = 'https://api.chucknorris.io/jokes/random';
+  return await get(URL);
 };
 
 const requestCategories = async () => {
-    const url = 'https://api.chucknorris.io/jokes/categories';
-    const response = await fetch(url);
-    const data = await response.json();
-    for (const item of data) {
-        console.log(chalk.white.bgGreen.bold(item));
-    }
+  const URL = 'https://api.chucknorris.io/jokes/categories';
+  return await get(URL);
 };
 
 const requestSpecificCategorie = async categorie => {
-    const url = `https://api.chucknorris.io/jokes/random?category=${categorie}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return console.log(chalk.white.bgGreen.bold(data.value));
-};
-
-const sum = (a, b) => {
-    return a + b;
+  const URL = `https://api.chucknorris.io/jokes/random?category=${categorie}`;
+  return await get(URL);
 };
 
 module.exports = {
-    requestJoke,
-    requestCategories,
-    requestSpecificCategorie,
-    sum,
+  requestJoke,
+  requestCategories,
+  requestSpecificCategorie,
 };
